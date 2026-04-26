ef893c695666604ec4f92c97a01753968c371c51
eyelss <stud0000200014@study.utmn.ru>
2026-04-27 01:43:34 +0500

    init commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	userpb "goapp/gen/goapp/user"
+	"log"
+
+	//productpb "goapp/gen/goapp/product"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type userServer struct {
+	userpb.UnimplementedUserServer
+}
+
+func (s *userServer) Check(ctx context.Context, in *userpb.UserRequest) (*userpb.UserResponse, error) {
+	log.Printf("Received: %v", in.GetReq())
+
+	return &userpb.UserResponse{Res: "OK" + in.GetReq()}, nil
+}
+
+func main() {
+	listener, _ := net.Listen("tcp", ":50051")
+	server := grpc.NewServer()
+	userpb.RegisterUserServer(server, &userServer{})
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
