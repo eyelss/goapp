4a1b30bb2728b008be081751e1316926fa460e5c
eyelss <stud0000200014@study.utmn.ru>
2026-05-16 15:16:58 +0500

    refactor

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -15,6 +15,69 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
+func handleRoot(c *echo.Context) error {
+	log.Println("Request root")
+
+	b, err := os.ReadFile("./dist/index.html")
+
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+	}
+
+	return c.Blob(http2.StatusOK, "text/html", b)
+}
+
+func handleResource(c *echo.Context) error {
+	path := c.Request().URL.Path
+	log.Printf("Request resource %s\n", path)
+	relativePath := "./dist" + path
+
+	ext := filepath.Ext(path)
+
+	var contentType string
+	switch ext {
+	case ".css":
+		contentType = "text/css"
+	case ".html":
+		contentType = "text/html"
+	case ".js":
+		contentType = "application/javascript"
+	case ".svg":
+		contentType = "image/svg+xml"
+	case ".png":
+		contentType = "image/png"
+	case ".jpg":
+	case ".jpeg":
+		contentType = "image/jpg"
+	}
+
+	if contentType == "" {
+		e := &ErrorResponse{
+			Message: "content type is not valid",
+		}
+
+		return c.JSON(http2.StatusNotAcceptable, e)
+	}
+
+	f, err := os.Open(relativePath)
+
+	if err != nil {
+		if _, ok := err.(*os.PathError); ok {
+			e := &ErrorResponse{
+				Message: "resource not found",
+			}
+
+			return c.JSON(http2.StatusNotFound, e)
+		}
+		log.Printf("Error reading file: %v", err)
+		return err
+	}
+
+	defer f.Close()
+
+	return c.Stream(http2.StatusOK, contentType, f)
+}
+
 func main() {
 	srv, err := http.New()
 
@@ -22,68 +85,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	srv.Echo.GET("/", func(c *echo.Context) error {
-		log.Println("Received message")
-		b, err := os.ReadFile("./dist/index.html")
+	srv.Echo.GET("/", handleRoot)
 
-		if err != nil {
-			log.Printf("Error reading file: %v", err)
-		}
-
-		return c.Blob(http2.StatusOK, "text/html", b)
-	})
-
-	srv.Echo.GET("/*", func(c *echo.Context) error {
-		path := c.Request().URL.Path
-		relativePath := "./dist" + path
-
-		ext := filepath.Ext(path)
-
-		var contentType string
-		switch ext {
-		case ".css":
-			contentType = "text/css"
-		case ".html":
-			contentType = "text/html"
-		case ".js":
-			contentType = "application/javascript"
-		case ".svg":
-			contentType = "image/svg+xml"
-		case ".png":
-			contentType = "image/png"
-		case ".jpg":
-		case ".jpeg":
-			contentType = "image/jpg"
-		}
-
-		if contentType == "" {
-			e := &ErrorResponse{
-				Message: "content type is not valid",
-			}
-
-			return c.JSON(http2.StatusNotAcceptable, e)
-		}
-
-		log.Printf("Received resource request %s\n", relativePath)
-
-		f, err := os.Open(relativePath)
-
-		if err != nil {
-			if _, ok := err.(*os.PathError); ok {
-				e := &ErrorResponse{
-					Message: "resource not found",
-				}
-
-				return c.JSON(http2.StatusNotFound, e)
-			}
-			log.Printf("Error reading file: %v", err)
-			return err
-		}
-
-		defer f.Close()
-
-		return c.Stream(http2.StatusOK, contentType, f)
-	})
+	srv.Echo.GET("/*", handleResource)
 
 	server.Run(srv)
 
